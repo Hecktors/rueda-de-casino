57d7faf38cfdcb918e11b5656682a95205d06e0f
Torsten <tohe.webdev@gmail.com>
2020-11-19 23:26:51 +0100

    Change interval time

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ export default function App() {
   function startMoveInterval() {
     intervalRef.current = setInterval(() => {
       setCurrentMove(getRandomMove)
-    }, 3000)
+    }, 10000)
   }
 
   // Destroy move-change-interval
