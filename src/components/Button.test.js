b0742add5a2d890aeeae65883272457b552f20eb
Torsten <tohe.webdev@gmail.com>
2020-12-05 10:03:48 +0100

    Add button test

--- src/components/Button.test.js
+++ src/components/Button.test.js
@@ -1,6 +1,7 @@
 import Button from './Button'
 import userEvent from '@testing-library/user-event'
 import { render } from '@testing-library/react'
+// import Home from '../pages/Home'
 
 describe('Button', () => {
   it('renders button', () => {
@@ -22,3 +23,16 @@ describe('Button', () => {
     expect(onClickMock).toHaveBeenCalled()
   })
 })
+
+// describe('Home Buttons', () => {
+//   it.todo('renders button', () => {
+//     const { getByTestId } = render(<Home />)
+//     const button = getByTestId('button')
+//     expect(button).toBeInTheDocument()
+//   })
+// })
+
+// using it.todo
+// Home
+// Test: Button task = 'play'
+// Test: Button onClick = 'history.push('/session')
