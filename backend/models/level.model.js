251ee3553398367b974665c9df556350d6be804c
Torsten <tohe.webdev@gmail.com>
2020-12-06 21:04:22 +0100

    Add DB models

---
+++ backend/models/level.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema();
+
+const levelSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minLength: 1,
+  },
+});
+
+const Level = new mongoose.model("Level", levelSchema);
+
+module.exports = Level;
