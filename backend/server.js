9752b4e649d9a244289d2c21bbaa677ab5aff0bf
Torsten <tohe.webdev@gmail.com>
2020-12-06 20:43:11 +0100

    Clean up sever.js

--- backend/server.js
+++ backend/server.js
@@ -23,17 +23,6 @@ connection.once("open", () =>
   console.log("MongoDB connection established successfully")
 );
 
-// app.get("/api/levels", async (req, res) => {
-//   var gtts = new gTTS("Buinvinido a la salsa app", "es-us");
-//   gtts.save("data/audio/test.mp3", function (err, result) {
-//     if (err) {
-//       throw new Error(err);
-//     }
-//     console.log("Success! Open file /data/audio/test.mp3 to hear result.");
-//   });
-//   res.sendFile(__dirname + "/data/pensum.json");
-// });
-
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
