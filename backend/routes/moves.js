16b362720f431a249d5b01ba2c8135cef82fb5bd
Torsten <tohe.webdev@gmail.com>
2020-12-07 12:03:44 +0100

    Add delete and update move logic

--- backend/routes/moves.js
+++ backend/routes/moves.js
@@ -1,22 +1,25 @@
 const router = require("express").Router();
-let Move = require("../models/move.model");
-const buildAudio = require("../services/buildAudio");
+const fs = require("fs");
+const Move = require("../models/move.model");
+// const Level = require("../models/level.model");
+const addAudio = require("../services/addAudio");
+const deleteAudio = require("../services/deleteAudio");
 
+// Get all moves
 router.route("/").get((req, res) => {
   Move.find()
     .then((moves) => res.json(moves))
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route("/add").post((req, res) => {
+// Add a move
+router.route("/add").post(async (req, res) => {
   const name = req.body.name.toLowerCase().trim().replace(/\s+/g, " ");
   const levelID = req.body.levelID;
   const steps = req.body.steps;
   const audioName = name.replace(/\s/g, "_") + ".mp3";
   const videoID = req.body.videoID;
   const videoStart = req.body.videoStart;
-
-  buildAudio(name, audioName);
 
   const newMove = new Move({
     name,
@@ -29,7 +32,53 @@ router.route("/add").post((req, res) => {
 
   newMove
     .save()
-    .then(() => res.json("Move added successfully!"))
+    .then(() => {
+      addAudio(name, audioName);
+      res.json("Move added successfully!");
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+// Get a move
+router.route("/:id").get((req, res) => {
+  Move.findById(req.params.id)
+    .then((move) => res.json(move))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+// Delete move
+router.route("/:id").delete((req, res) => {
+  Move.findByIdAndDelete(req.params.id)
+    .then((move) => {
+      deleteAudio(move.audioName);
+      res.json("Move deleted.");
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+// Update move
+router.route("/update/:id").post((req, res) => {
+  Move.findById(req.params.id)
+    .then((move) => {
+      const prevAudioName = move.audioName;
+      move.name = req.body.name.toLowerCase().trim().replace(/\s+/g, " ");
+      move.levelID = req.body.levelID;
+      move.steps = req.body.steps;
+      move.audioName = req.body.name.replace(/\s/g, "_") + ".mp3";
+      move.videoID = req.body.videoID;
+      move.videoStart = req.body.videoStart;
+
+      move
+        .save()
+        .then(() => {
+          if (prevAudioName !== move.audioName) {
+            deleteAudio(prevAudioName);
+            addAudio(move.name, move.audioName);
+          }
+          res.json("Move updated!");
+        })
+        .catch((err) => res.status(400).json("Error: " + err));
+    })
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
