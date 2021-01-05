dbf0a0f066f9dc2b4fcd9bc3b6ae63ad7b3c3c9b
Torsten <tohe.webdev@gmail.com>
2021-01-05 22:45:53 +0100

    Refactor settingspage

--- client/src/settings/useUserInput.js
+++ client/src/settings/useUserInput.js
@@ -1,28 +1,15 @@
 const initState = {
-  selectedMoveIDs: [],
   speed: 2900,
   isSongActive: true,
 }
 
-export default function useUserInput(levels, appState, setAppState) {
-  const selectedMoves = appState.selectedMoveIDs
-    ? levels
-        .map((level) => level.moves)
-        .filter((move) => appState.selectedMoveIDs.includes(move._id))
-    : []
-
+export default function useUserInput(appState, setAppState) {
   function updateAppState(event) {
     const { name, value, checked } = event.target
     appStateHandler[name](value, checked)
   }
 
   const appStateHandler = {
-    move: (value) => {
-      const updatedMoveIDs = appState.selectedMoveIDs.includes(value)
-        ? appState.selectedMoveIDs.filter((moveID) => moveID !== value)
-        : [...appState.selectedMoveIDs, value]
-      setAppState({ ...appState, selectedMoveIDs: updatedMoveIDs })
-    },
     speed: (value) => setAppState({ ...appState, speed: Number(value) }),
     songActivity: (_, checked) => {
       setAppState({ ...appState, isSongActive: checked })
@@ -33,5 +20,5 @@ export default function useUserInput(levels, appState, setAppState) {
     setAppState(initState)
   }
 
-  return { selectedMoves, updateAppState, resetAppState }
+  return { updateAppState, resetAppState }
 }
