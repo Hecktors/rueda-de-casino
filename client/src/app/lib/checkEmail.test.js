b658774568173856b0083d7b9905b580eeb98532
hecktors <tohe.webdev@gmail.com>
2021-01-20 13:36:33 +0100

    Update lib functions test descriptions

--- client/src/app/lib/checkEmail.test.js
+++ client/src/app/lib/checkEmail.test.js
@@ -1,23 +1,23 @@
 import checkEmail from './checkEmail'
 
 describe('Email check function', () => {
-    it('should be a valid email address', () =>  {
+    it('is a valid email address', () =>  {
         expect(checkEmail('test@gmail.com')).toBeTruthy()
     })
 
-    it('should be falsy if doesn\'t contain local part', () =>  {
+    it('doesn\'t contain local part', () =>  {
         expect(checkEmail('@gmail.com')).toBeFalsy()
     })
 
-    it('should be falsy if doesn\'t contain at sign', () =>  {
+    it('doesn\'t contain at sign', () =>  {
         expect(checkEmail('testgmail.com')).toBeFalsy()
     })
 
-    it('should be falsy if doesn\'t contain period between hostname and top-level.domain ', () =>  {
+    it('doesn\'t contain period between hostname and top-level.domain ', () =>  {
         expect(checkEmail('test@gmailcom')).toBeFalsy()
     })
 
-    it('should be falsy if doesn\'t contain valid top-level.domain', () =>  {
+    it('doesn\'t contain valid top-level.domain', () =>  {
         expect(checkEmail('test@gmail.c')).toBeFalsy()
     })
 })
--- client/src/app/lib/checkPassword.test.js
+++ client/src/app/lib/checkPassword.test.js
@@ -2,23 +2,23 @@ import { isTypeReferenceNode } from 'typescript'
 import checkPassword from './checkPassword'
 
 describe('Password check function', () => {
-  it('should pass if have contains lowercase letters, uppercase letters, numbers, min length of 8', () => 
+  it('contains lowercase letters, uppercase letters, numbers, min length of 8', () => 
     expect(checkPassword('abCD1234')).toBeTruthy()
   )
 
-  it('should\'t pass if min length lower than 8', () => 
+  it('has min length lower than 8', () => 
     expect(checkPassword('abCD123')).toBeFalsy()
   )
 
-  it('should pass if doesn\'t contains lowercase letters', () => 
+  it('contains lowercase letters', () => 
   expect(checkPassword('ABCD1234')).toBeFalsy()
   )
 
-  it('should pass if doesn\'t contains uppercase letters', () => 
+  it('contains uppercase letters', () => 
   expect(checkPassword('abcd1234')).toBeFalsy()
   )
 
-  it('should pass if doesn\'t contains numers', () => 
+  it('contains numers', () => 
   expect(checkPassword('abcd1234')).toBeFalsy()
   )
 })
