03c955797005a4eb14f6920ec9db455ba4d6e9e9
hecktors <tohe.webdev@gmail.com>
2021-03-31 08:33:22 +0200

    Revert "Fix useEffect useAudios and useEffect useLevels, fix inputPlaySong unit test"
    This reverts commit 3fecbac89d5ec70ee4fed6e72eada96721b99e01.

--- client/src/settings/InputPlaySong/InputPlaySong.test.js
+++ client/src/settings/InputPlaySong/InputPlaySong.test.js
@@ -5,6 +5,7 @@ describe('InputPlaySong', () => {
   test('renders checked checkbox', () => {
     render(<InputPlaySong isSongActive={true} updateAppState={() => {}} />)
     const checkbox = screen.getByRole('checkbox', {
+      name: /play learning song/i,
       hidden: true,
     })
     expect(checkbox).toBeChecked()
