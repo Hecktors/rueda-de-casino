18b1a0df305f042ecc51e3dc3889f7a0b784df97
hecktors <tohe.webdev@gmail.com>
2021-04-08 09:23:52 +0200

    Move default song to public folder

--- client/src/settings/InputPlaySong/InputPlaySong.test.js
+++ client/src/settings/InputPlaySong/InputPlaySong.test.js
@@ -5,7 +5,6 @@ describe('InputPlaySong', () => {
   test('renders checked checkbox', () => {
     render(<InputPlaySong isSongActive={true} updateAppState={() => {}} />)
     const checkbox = screen.getByRole('checkbox', {
-      name: /play learning song/i,
       hidden: true,
     })
     expect(checkbox).toBeChecked()
