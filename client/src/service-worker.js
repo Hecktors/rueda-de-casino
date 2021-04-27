09ce002f258161c255453bd3fa57af418431cbb7
hecktors <tohe.webdev@gmail.com>
2021-04-27 18:01:07 +0200

    Update manifest and  icons

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -76,7 +76,7 @@ self.addEventListener('message', (event) => {
 
 precacheAndRoute([{ url: '/logo/favicon-196.png', revision: null }])
 precacheAndRoute([{ url: '/assets/video/rueda.mp4', revision: null }])
-precacheAndRoute([{ url: '/assets/audio/Uno_dos_tres.mp3', revision: null }])
+// precacheAndRoute([{ url: '/assets/audio/Uno_dos_tres.mp3', revision: null }])
 
 // Cache Google Fonts with a stale-while-revalidate strategy, with
 // a maximum number of entries.
@@ -87,6 +87,23 @@ registerRoute(
   new StaleWhileRevalidate({
     cacheName: 'google-fonts',
     plugins: [new ExpirationPlugin({ maxEntries: 20 })],
+  })
+)
+
+// Cache page navigations (html) with a Network First strategy
+registerRoute(
+  // Check to see if the request is a navigation to a new page
+  ({ request }) => request.mode === 'navigate',
+  // Use a Network First caching strategy
+  new CacheFirst({
+    // Put all cached files in a cache named 'pages'
+    cacheName: 'pages',
+    plugins: [
+      // Ensure that only requests that result in a 200 status are cached
+      new CacheableResponsePlugin({
+        statuses: [200],
+      }),
+    ],
   })
 )
 
